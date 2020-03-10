1ae9c4c094257229acd60e772a88bbabc15ac6f6
WanLittle <2621996061@qq.com>
2020-03-10 17:03:40 +0800

    feat: 常用cpp头文件

--- src/oj_main.cpp
+++ src/oj_main.cpp
@@ -1,2 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
 
+int main2(void)
+{
+    int N;
+    scanf("%d", &N);
+    while (N--) {
 
+    }
+    return 0;
+}
