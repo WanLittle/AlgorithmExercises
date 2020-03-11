2324c42bd94782fc988073446e26e63e4bf1786d
WanLittle <2621996061@qq.com>
2020-03-11 09:02:07 +0800

    镜像二叉树

---
+++ src/SwordOffer/S_27_MirrorBinTree.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../BASE.h"
+
+class S_27_MirrorBinTree : public BASE {
+public:
+	virtual void main()
+	{
+	}
+    struct TreeNode
+    {
+        int val;
+        struct TreeNode *left;
+        struct TreeNode *right;
+        TreeNode(int x) : val(x), left(NULL), right(NULL) { }
+    };
+    void Mirror(TreeNode *pRoot) 
+    {
+        if (pRoot == nullptr)
+            return;
+        if (pRoot->left == nullptr && pRoot->right == nullptr)
+            return;
+        TreeNode *t = pRoot->left;
+        pRoot->left = pRoot->right;
+        pRoot->right = t;
+        Mirror(pRoot->left);
+        Mirror(pRoot->right);
+        std::fmin
+    }
+};
