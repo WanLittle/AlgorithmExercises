ac3f8237cb82652d592faf20243ac0e16b6dcd8e
WanLittle <2621996061@qq.com>
2020-04-20 20:03:02 +0800

    顺时针打印矩阵

--- src/SwordOffer/S_29_SpiralOrder.h
+++ src/SwordOffer/S_29_SpiralOrder.h
@@ -6,19 +6,18 @@ public:
 	virtual void main()
 	{
 	}
-public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) 
     {
         if (!matrix.size()) return {};
         if (!matrix[0].size()) return matrix[0];
 
-        // 表示方向:→、↓、←、↑
+        // 遍历方向:→、↓、←、↑
         vector<int> di = { 0,1,0,-1 };
         vector<int> dj = { 1,0,-1,0 };
 
         vector<int> Res;
         int row = matrix.size(), col = matrix[0].size();
-        int num = row * col;    //矩阵元素个数
+        int num = row * col;
         int x = 0, y = 0;   //待访问元素坐标
         vector<vector<int>> flag(row, vector<int>(col, 1)); //是否访问过该数组
         int k = 0;  //遍历方向        
