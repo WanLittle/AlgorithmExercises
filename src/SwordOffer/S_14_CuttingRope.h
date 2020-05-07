a42f7a7564802829a2dca8b7962b8283c5541f69
WanLittle <2621996061@qq.com>
2020-05-07 11:05:43 +0800

    剪绳子

---
+++ src/SwordOffer/S_14_CuttingRope.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "../BASE.h"
+
+/*
+给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+每段绳子的长度记为 k[0],k[1]...k[m] 。请问 k[0]*k[1]*...*k[m] 可能的最大乘积是多少？
+例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+*/
+class S_14_CuttingRope : public BASE {
+public:
+	virtual void main()
+	{
+	}
+    int cuttingRope(int n) 
+    {
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        for (int n_p = 4; n_p <= n; ++n_p) 
+        {
+            for (int i = 1; i <= n_p - 1; ++i) 
+            {
+                dp[n_p] = max(dp[n_p], i * max(n_p - i, dp[n_p - i]));
+            }
+        }
+        return dp[n];
+    }
+};
--- src/SwordOffer/SwordOffer_header.h
+++ src/SwordOffer/SwordOffer_header.h
@@ -15,7 +15,7 @@
 #include "S_11_MinArray.h" // 旋转数组的最小数字
 #include "S_12_Exist.h" // 矩阵中的路径
 #include "S_13_MovingCount.h" // 机器人的运动范围
-#include "S_14.h"
+#include "S_14_CuttingRope.h" // 剪绳子
 #include "S_15_CountBitOne.h" //二进制中1的个数
 #include "S_16_MyPow.h"
 #include "S_18_DeleteListNode.h" // 删除链表的节点
