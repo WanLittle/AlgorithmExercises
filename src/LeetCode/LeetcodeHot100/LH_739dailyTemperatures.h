9d01cd232a246656276252df0306f02d53edf5fe
WanLittle <2621996061@qq.com>
2020-08-23 11:45:37 +0800

    单调栈-LH_739dailyTemperatures

---
+++ src/LeetCode/LeetcodeHot100/LH_739dailyTemperatures.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../../BASE.h"
+
+
+class LH_739dailyTemperatures : public BASE {
+public:
+	virtual void main()
+	{
+	}
+    vector<int> dailyTemperatures(vector<int>& T) {
+        vector<int> result(T.size(), 0);
+        stack<int> monostack;
+        for (int i = 0; i < T.size(); ++i)
+        {
+            while (!monostack.empty() && T[monostack.top()] < T[i])
+            {
+                result[monostack.top()] = i;
+                monostack.pop();
+            }
+            monostack.push(i);
+        }
+        return result;
+    }
+};
