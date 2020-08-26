adbc98ee238946f83ea0c62c62e1c8d04225c468
WanLittle <2621996061@qq.com>
2020-08-26 17:40:01 +0800

    leetcode 11

---
+++ src/LeetCode/LeetcodeHot100/L_11_maxArea.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../../BASE.h"
+
+
+class L_11_maxArea : public BASE {
+public:
+	virtual void main()
+	{
+	}
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = min(height[l], height[r]) * (r - l);
+            ans = max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+        }
+        return ans;
+    }
+};
